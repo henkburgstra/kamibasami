18f485f0007c76375f6db2d4b1dcc7ad02bbcca6
Henk Burgstra <henk.burgstra@gmail.com>
2017-04-24 18:35:48 +0200

    unit test for storePage

--- controllers/webpage_test.go
+++ controllers/webpage_test.go
@@ -1,26 +1,49 @@
 package controllers
 
 import (
-	"reflect"
 	"testing"
+	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/henkburgstra/kamibasami/node"
 	"github.com/henkburgstra/kamibasami/service"
 )
 
+func startServer() {
+	router := gin.Default()
+	router.GET("/test", func(c *gin.Context) {
+		c.String(200, `
+		<html>
+			<head><title>Testonderwerp</title></head>
+			<body>
+				<h1>Eerste hoofdstuk</h1>
+				<p>Tekst</p>
+				<h2>Tweede hoofdstuk</h2>
+				<p>Meer tekst</p>
+			</body>
+		</html>`)
+	})
+	router.Run()
+}
+
 func Test_storePage(t *testing.T) {
+	svc := service.NewService()
+	svc.SetNodeRepo(node.NewMockNodeRepo())
+
+	go startServer()
+	time.Sleep(3 * time.Second)
+
 	type args struct {
 		svc  *service.Service
 		url  string
 		path string
 	}
 	tests := []struct {
-		name     string
-		args     args
-		wantPage *node.Webpage
-		wantErr  bool
+		name    string
+		args    args
+		wantErr bool
 	}{
-	// TODO: Add test cases.
+		{"valid path", args{svc: svc, url: "http://localhost:8080/test", path: "a/test/path"}, false},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -29,8 +52,9 @@ func Test_storePage(t *testing.T) {
 				t.Errorf("storePage() error = %v, wantErr %v", err, tt.wantErr)
 				return
 			}
-			if !reflect.DeepEqual(gotPage, tt.wantPage) {
-				t.Errorf("storePage() = %v, want %v", gotPage, tt.wantPage)
+			if gotPage.Name() != "Testonderwerp" {
+				t.Errorf("storePage() page name = '%s', want 'Testonderwerp'", gotPage.Name())
+				return
 			}
 		})
 	}
