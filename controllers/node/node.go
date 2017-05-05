3593c978852ce2c43968a6ee21058495ef6d8f37
Henk Burgstra <henk.burgstra@gmail.com>
2017-05-05 10:35:49 +0200

    working on templates

--- controllers/node/node.go
+++ controllers/node/node.go
@@ -5,10 +5,9 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/henkburgstra/kamibasami/service"
+	"github.com/henkburgstra/kamibasami/viewmodels"
 )
 
 func Home(svc *service.Service, c *gin.Context) {
-	c.HTML(http.StatusOK, "home.tmpl.html", gin.H{
-		"title": "Posts",
-	})
+	c.HTML(http.StatusOK, "home.tmpl.html", viewmodels.Viewmodel{Title: "test"})
 }
---
+++ viewmodels/viewmodel.go
@@ -0,0 +1,5 @@
+package viewmodels
+
+type Viewmodel struct {
+	Title string
+}
